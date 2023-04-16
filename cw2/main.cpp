385dbfe308ba2a3a6a31cae9ed94d812116e0c50
NANAnoo <602075355@qq.com>
2023-04-16 19:23:26 +0100

    init

---
+++ cw2/main.cpp
@@ -0,0 +1,66 @@
+#include <tuple>
+#include <chrono>
+#include <limits>
+#include <vector>
+#include <stdexcept>
+
+#include <cstdio>
+#include <cassert>
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
+
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+#include <volk/volk.h>
+
+#if !defined(GLM_FORCE_RADIANS)
+#	define GLM_FORCE_RADIANS
+#endif
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "../labutils/to_string.hpp"
+#include "../labutils/vulkan_window.hpp"
+
+#include "../labutils/angle.hpp"
+using namespace labutils::literals;
+
+#include "../labutils/error.hpp"
+#include "../labutils/vkutil.hpp"
+#include "../labutils/vkimage.hpp"
+#include "../labutils/vkobject.hpp"
+#include "../labutils/vkbuffer.hpp"
+#include "../labutils/allocator.hpp" 
+namespace lut = labutils;
+
+#include "baked_model.hpp"
+
+
+namespace
+{
+
+
+	// Local types/structures:
+
+	// Local functions:
+}
+
+int main() try
+{
+	//TODO-implement me.
+
+
+	return 0;
+}
+catch( std::exception const& eErr )
+{
+	std::fprintf( stderr, "\n" );
+	std::fprintf( stderr, "Error: %s\n", eErr.what() );
+	return 1;
+}
+
+
+//EOF vim:syntax=cpp:foldmethod=marker:ts=4:noexpandtab: 
